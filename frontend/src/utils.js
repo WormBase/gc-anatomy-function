52d61c9dc51b3406523b8afaa5817780d925c42b
Valerio Arnaboldi <valearna@caltech.edu>
2020-08-19 11:11:56 -0700

    simplifying diff functions

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,15 +1,18 @@
 export const entitiesAreEqual = (e1, e2) => {
-    return e1.value === e2.value && e1.modId === e2.modId && (e1.options === undefined && e2.options === undefined || Object.entries(e1.options).every(([k, v]) => e2.options[k] === v))
+    if (e1.value !== undefined) {
+        return e1.value === e2.value && e1.modId === e2.modId && (e1.options === undefined && e2.options === undefined || Object.entries(e1.options).every(([k, v]) => e2.options[k] === v))
+    } else {
+        return e1 === e2;
+    }
 }
 
 export const anatomyFnctAnnotationsAreEqual = (a1, a2) => {
-    return a1.annotationId === a2.annotationId
-        && entitiesAreEqual(a1.phenotype, a2.phenotype) && entitiesAreEqual(a1.gene, a2.gene)
-        && a1.anatomyTerms.every((t, idx) => {return entitiesAreEqual(t, a2.anatomyTerms[idx])})
-        && a1.remarks.every((r, idx) => {return r === a2.remarks[idx]}) && a1.genotypes.every((r, idx) => {return r === a2.genotypes[idx]})
-        && a1.noctuamodels.every((r, idx) => {return r === a2.noctuamodels[idx]})
-        && a1.authorstatements.every((r, idx) => {return r === a2.authorstatements[idx]}) && a1.assay.value === a2.assay.value
-        && a1.evidence === a2.evidence && a1.dateAssigned === a2.dateAssigned;
+    return a1.annotationId === a2.annotationId && Object.entries(a1).every(([a1key, a1value]) => {
+        if (Array.isArray(a1value)) {
+            a1.anatomyTerms.every((t, idx) => {return entitiesAreEqual(t, a2[a1key][idx])})
+        } else {
+            return entitiesAreEqual(a1value, a2[a1key]);
+        }});
 }
 
 export const getAnnotationsDiffStatus = (oldAnnotations, newAnnotations) => {
